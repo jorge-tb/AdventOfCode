cf60a30e2282606a33e56406b2630b9441f6eccf
Jorge Tome Blanco <jorgetomeb@gmail.com>
2025-12-22 01:54:47 +0100

    feat(day5): add solution for the part two.

--- 2025/days/5/5.js
+++ 2025/days/5/5.js
@@ -10,6 +10,28 @@ class Range {
         return this.min <= n && n <= this.max;
     }
 
+    combine(range) {
+        // min1,max1  min,max  min2,max2
+        if (range.max < this.min || range.min > this.max)
+            return false;
+
+        if (range.min < this.min && range.max <= this.max) 
+            this.min = range.min;
+        else if (range.min >= this.min && range.max > this.max)
+            this.max = range.max;
+        else if (range.min <= this.min && range.max >= this.max) {
+            this.max = range.max;
+            this.min = range.min;
+        }
+        return true;
+    }
+
+    contains(range) {
+        if (range.min >= this.min && range.max <= this.max)
+            return true;
+        return false;
+    }
+
     static parse(formatted) {
         const [minString, maxString] = formatted.split('-');
         return new Range(Number(minString), Number(maxString));
@@ -52,6 +74,30 @@ function readExample() {
     };
 }
 
+/**
+ * Those ranges which overlap others are combined until obtaining an array of ranges equivalent to the original one.
+ * @param {Array<Range>} ranges 
+ */
+function condense(ranges) {
+    let condensed = [...ranges];
+    let done = false;
+    while (!done) {
+        for (let i = 0; i < condensed.length; i++) {
+            for (let j = 0; j < condensed.length; j++) {
+                if (i === j) continue;
+                if (condensed[j] && condensed[i] && condensed[i].combine(condensed[j])) {
+                    condensed[j] = null;
+                }
+            }
+        }
+
+        done = condensed.every(e => e);
+        condensed = condensed.filter(e => e);
+    }
+
+    return condensed;
+}
+
 function answer_part1() {
     const input = readInput();
     const freshIDs = [];
@@ -62,4 +108,15 @@ function answer_part1() {
     console.log(`Fresh IDs are: ${freshIDs} | length: ${freshIDs.length}`);
 }
 
+function answer_part2() {
+    const { ranges } = readInput();
+    const condensedRanges = condense(ranges);
+    const total = condensedRanges.reduce((prev, r) => {
+        return prev += r.max - r.min + 1;
+    }, 0);
+    console.log(`Fresh IDs are: ${total}`);
+}
+
 answer_part1();
+
+answer_part2();
