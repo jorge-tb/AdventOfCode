36c79a7c8cf33c8bab9c5069bb129b58e42b3f4b
Jorge Tome Blanco <jorgetomeb@gmail.com>
2025-12-24 01:33:06 +0100

    feat(day6): add solution of part two.

--- 2025/days/6/6.js
+++ 2025/days/6/6.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-function readInput() {
+function readInput_v1() {
     const inputText = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
     const matrix = [];
     inputText
@@ -16,24 +16,80 @@ function readInput() {
     return matrix;
 }
 
-function answer_part1() {
+function readInput_v2() {
+    const inputText = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
+    const matrix = [];
+    const split = inputText.split(/\r?\n/).map(row => row.split(''));
+    let number = [];
+    let operator;
+    let row = 0;
+    for (let i = 0; i < split[0].length; i++) {
+        for (let j = 0; j < split.length; j++) {
+            if (split[j][i] === '*' || split[j][i] === '+')
+                operator = split[j][i];
+            else if (split[j][i] !== ' ') // === not equal to ''
+                number.push(split[j][i])
+        }
+
+        if (matrix.length < (row + 1))
+            matrix.push([]);
+        
+        if (number.length > 0) {
+            matrix[row].push(
+                Number(
+                    number.reduce((prev, curr) => prev + curr), ''));
+            row++;
+        } 
+        
+        if (!number.length || i === split[0].length - 1) {
+            Array(matrix.length - 1).fill(
+                operator === '*' ? 1 : 0
+            ).forEach((v, index) => {
+                if (matrix[index].length < matrix[0].length) {
+                    matrix[index].push(v);
+                    row++;
+                }
+            });
+            matrix[row].push(operator);
+            row = 0;
+        }
+
+        number.length = 0;
+    }
+    return matrix;
+}
+
+function resolveMatrix(matrix) {
     const multiply = (numbers) => numbers.reduce((prev, curr) => prev * curr, 1);
     const sum = (numbers) => numbers.reduce((prev, curr) => prev + curr, 0);
 
     let total = 0;
     let numbers = [];
-    const matrixProblem = readInput();
-    for (let i = 0; i < matrixProblem[0].length; i++) {
-        for (let j = 0; j < matrixProblem.length; j++) {
-            if (j < matrixProblem.length - 1) {
-                numbers.push(matrixProblem[j][i]);
+    for (let i = 0; i < matrix[0].length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            if (j < matrix.length - 1) {
+                numbers.push(matrix[j][i]);
             } else {
-                total += matrixProblem[j][i] === '*' ? multiply(numbers) : sum(numbers);
+                total += matrix[j][i] === '*' ? multiply(numbers) : sum(numbers);
                 numbers.length = 0;
             }
         }
     }
-    console.log(`Total is ${total}`);
+    return total;
+}
+
+function answer_part1() {
+    const matrix = readInput_v1();
+    const total = resolveMatrix(matrix);
+    console.log(`(v1) Total is ${total}`);
+}
+
+function answer_part2() {
+    const matrix = readInput_v2();
+    const total = resolveMatrix(matrix);
+    console.log(`(v2) Total is ${total}`);
 }
 
 answer_part1();
+
+answer_part2();
