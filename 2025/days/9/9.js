ef1f4b3fdc066963c986a162c622539e20299366
Jorge Tome Blanco <jorgetomeb@gmail.com>
2026-01-16 15:45:17 +0100

    feat: add getLimitEdges(graph).

--- 2025/days/9/9.js
+++ 2025/days/9/9.js
@@ -1,5 +1,87 @@
 import fs from 'fs';
 
+class Point {
+    /**
+     * Point constructor.
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * If equals return true, otherwise false.
+     * @param {Point} point 
+     * @returns 
+     */
+    equals(point) {
+        return point.x === this.x && point.y === this.y;
+    }
+
+
+    isLowerThan(point) {
+        if (this.x < point.x)
+            return true;
+        else if (this.x === point.x && this.y < point.y)
+            return true;
+        else
+            return false;
+    }
+
+    toJSON() {
+        return JSON.stringify({ x: this.x, y: this.y });
+    }
+}
+
+class Edge {
+    /**
+     * Edge constructor.
+     * @param {Point} point1 
+     * @param {Point} point2 
+     */
+    constructor(point1, point2) {
+        if (point1.isLowerThan(point2))
+            this.value = [point1, point2];
+        else
+            this.value = [point2, point1];
+    }
+
+    /**
+     * If equals return true, otherwise false.
+     * @param {Edge} edge 
+     */
+    equals(edge) {
+        return this.value[0].equals(edge.value[0]) &&
+        this.value[1].equals(edge.value[1]);
+    }
+
+    isRow() {
+        return this.value[0].y === this.value[1].y;
+    }
+
+    isColumn() {
+        return this.value[0].x === this.value[1].x;
+    }
+
+    minX() {
+        return this.value[0].x;
+    }
+
+    maxX() {
+        return this.value[1].x;
+    }
+
+    minY() {
+        return this.value[0].y < this.value[1].y ? this.value[0].y : this.value[1].y;
+    }
+
+    maxY() {
+        return this.value[0].y < this.value[1].y ? this.value[1].y : this.value[0].y;
+    }
+}
+
 function readInput() {
     const textInput = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
     const input = textInput.split('\n').map(line => {
@@ -10,16 +92,8 @@ function readInput() {
 }
 
 function readExample() {
-    const textExample = 
-        `7,1
-        11,1
-        11,7
-        9,7
-        9,5
-        2,5
-        2,3
-        7,3`;
-    const example = textExample.split('\n').map(line => {
+    const textExample = `7,1:11,1:11,7:9,7:9,5:2,5:2,3:7,3`;
+    const example = textExample.split(':').map(line => {
         const [x, y] = line.split(',');
         return { x: +x, y: +y };
     });
@@ -34,7 +108,7 @@ function area(point1, point2) {
  * Find out which pair of points can generate the biggest rectangle.
  * @param {Array<{ x: number, y: number }>} points 
  */
-function findBiggestRectangle(points) {
+function findBiggestRectangle_v1(points) {
     let biggest = { area: 0 };
     let currentArea;
     for (let i = 0; i < points.length; i++) {
@@ -44,19 +118,17 @@ function findBiggestRectangle(points) {
                 biggest = { area: currentArea, points: [points[i], points[j]] };
         }
     }
-
     return biggest;
 }
 
 const columnConnections = new Map();
 const rowConnections = new Map();
 
-
 /**
  * 
  * @param {Array<{ x: number, y: number }>} points 
  */
-function c(points) {
+function findBiggestRectangle_v2(points) {
     let rectangles = [];
     for (let i = 0; i < points.length; i++) {
         upsertConnection(points[i]);
@@ -71,10 +143,80 @@ function c(points) {
     // Rectangles sorted in descending order by area
     rectangles.sort((pointA, pointB) => pointB.area - pointA.area);
 
-    console.log(`rectangles = ${JSON.stringify(rectangles[0])} |${JSON.stringify(rectangles[rectangles.length - 1])} `);
+    // Build graph (It's supposing all points can generate the same graph)
+    const graph = buildGraph(new Point(points[0].x, points[0].y));
 
-    const biggestValid = rectangles.find(rectangle => isValid(rectangle));
-    console.log(`biggest valid rectangle is: ${JSON.stringify(biggestValid)}`);
+    // Get graph limits
+    const limits = getLimitEdges(graph);
+
+    rectangles.find(rectangle => {
+        const pointA = new Point(rectangle.points[0].x, rectangle.points[0].y);
+        const pointB = new Point(rectangle.points[1].x, rectangle.points[1].y);
+        const pointC = new Point(rectangle.points[0].x, rectangle.points[1].y);
+        const pointD = new Point(rectangle.points[1].x, rectangle.points[0].y);
+
+        const rectangleEdges = [
+            new Edge(pointA, pointD),
+            new Edge(pointC, pointB),
+            new Edge(pointA, pointC),
+            new Edge(pointD, pointB)
+        ];
+    });
+
+
+    // console.log(`rectangles = ${JSON.stringify(rectangles[0])} |${JSON.stringify(rectangles[rectangles.length - 1])} `);
+
+    // const biggestRectangle = rectangles.find(rectangle => {
+    //     const pointA = new Point(rectangle.points[0].x, rectangle.points[0].y);
+    //     const pointB = new Point(rectangle.points[1].x, rectangle.points[1].y);
+    //     const pointC = new Point(rectangle.points[0].x, rectangle.points[1].y);
+    //     const pointD = new Point(rectangle.points[1].x, rectangle.points[0].y);
+    //     const rectangleEdges = [
+    //         new Edge(pointA, pointD),
+    //         new Edge(pointC, pointB),
+    //         new Edge(pointA, pointC),
+    //         new Edge(pointD, pointB)
+    //     ];
+    //     const graph = buildGraph(pointA);
+    //     if (graph.length > 1 && graph.every(e => Array.isArray(e))) {
+    //         console.log(`point ${JSON.stringify(pointA)} generates more than one graph`);
+    //         return false;
+    //     } else {
+    //         return rectangleEdges.every(e => isIn(e, graph));
+    //     }
+    // });
+
+    // console.log(`biggest rectangle: ${JSON.stringify(biggestRectangle)}`);
+
+    // return biggestRectangle;
+}
+
+/**
+ * 
+ * @param {Array<Edge>} graph
+ * @returns {{ top: Edge, bottom: Edge, right: Edge, left: Edge }}
+ */
+function getLimitEdges(graph) {
+    const limits = {
+        top: null,
+        bottom: null,
+        right: null,
+        left: null
+    };
+    graph.forEach(e => {
+        if (e.isColumn()) {
+            if (e.maxX() > (limits.right?.maxX() ?? -Infinity))
+                limits.right = e;
+            if (e.minX() < (limits.left?.minX() ?? Infinity))
+                limits.left = e;
+        } else {
+            if (e.maxY() > (limits.top?.maxY() ?? -Infinity))
+                limits.top = e;
+            if (e.minY() < (limits.bottom?.minY() ?? Infinity))
+                limits.bottom = e;
+        }
+    });
+    return limits;
 }
 
 /**
@@ -104,7 +246,7 @@ function upsertRowConnection(point) {
 
 /**
  * Upsert column connection.
- * @param {*} point 
+ * @param {{ x: number, y: number }} 
  */
 function upsertColumnConnection(point) {
     if (columnConnections.has(point.x)) {
@@ -119,43 +261,100 @@ function upsertColumnConnection(point) {
 }
 
 /**
- * Return true is rectangle is valid (is composed by red and green tails), otherwise false.
- * @param {{ area: number, points: Array<{ x: number, y: number }>}} rectangle 
+ * Based on point coordinates and edges connections a graph of these edges is builded.
+ * @param {Point} point 
+ * @param {Array<Edge>} graph
+ * @returns {Array<Edge>}
  */
-function isValid(rectangle) {
-    console.log(`(isValid) rectange = ${JSON.stringify(rectangle)}`);
-    let [point1, point2] = rectangle.points;
-    let connection;
+function buildGraph(point, graph = []) {
+    let connectionPoint;
+    if (graph.length === 0) {
+        let { min: minY, max: maxY } = columnConnections.get(point.x);
+        let { min: minX, max: maxX } = rowConnections.get(point.y);
+        let columnEdge = new Edge(new Point(point.x, minY), new Point(point.x, maxY));
+        let rowEdge = new Edge(new Point(minX, point.y), new Point(maxX, point.y));
+        connectionPoint = tryConnectEdges(columnEdge, rowEdge);
 
-    connection = columnConnections.get(point1.x);
-    if (connection.min > point1.y || connection.min > point2.y) return false;
-    if (connection.max < point1.y || connection.max < point2.y) return false;
+        if (columnEdge.equals(rowEdge)) {
+            // It's a lonely unconnected point
+            graph.push(columnEdge);
+        } else if (connectionPoint) {
+            // Ensure: [(P_a-P_b), (P_b-P_c)] instead of [(P_b-P_c), (P_a-P_b)]
+            if (columnEdge.value[0].equals(connectionPoint))
+                graph = [rowEdge, columnEdge];
+            else
+                graph = [columnEdge, rowEdge];
+        } else {
+            // If row & column edges are disconnected, create a different graph for each one
+            const graph1 = buildGraph(null, [columnEdge]);
+            const graph2 = buildGraph(null, [rowEdge]);
+            
+            if (graph1.some(edge => edge.equals(graph2[0])))
+                return graph1; // If they both converged in the same graph, return only one
+            else
+                return [graph1, graph2]; // Else, return both
+        }
+    } else {
+        // If graph contains elements
+        let lastEdge = graph.at(graph.length - 1);
+        let nextPoint = point?.equals(lastEdge.value[0]) ?? false  ? lastEdge.value[1] : lastEdge.value[0];
+        let columnEdge, rowEdge;
 
-    connection = columnConnections.get(point2.x);
-    if (connection.min > point1.y || connection.min > point2.y) return false;
-    if (connection.max < point1.y || connection.max < point2.y) return false;
+        let { min: minY, max: maxY } = columnConnections.get(nextPoint.x);
+        columnEdge = new Edge(new Point(nextPoint.x, minY), new Point(nextPoint.x, maxY));
 
-    connection = rowConnections.get(point1.y)
-    if (connection.min > point1.x || connection.min > point2.x) return false;
-    if (connection.max < point1.x || connection.max < point2.x) return false;
+        let { min: minX, max: maxX } = rowConnections.get(nextPoint.y);
+        rowEdge = new Edge(new Point(minX, nextPoint.y), new Point(maxX, nextPoint.y));
 
-    connection = rowConnections.get(point2.y)
-    if (connection.min > point1.x || connection.min > point2.x) return false;
-    if (connection.max < point1.x || connection.max < point2.x) return false;
+        if (lastEdge.equals(columnEdge) && lastEdge.equals(rowEdge)) {
+            return graph;
+        } else if (lastEdge.equals(columnEdge)) {
+            if (rowEdge.equals(graph[0])) {
+                return graph;
+            }
+            graph.push(rowEdge);
+        } else if (lastEdge.equals(rowEdge)) {
+            if (columnEdge.equals(graph[0])) {
+                return graph;
+            }
+            graph.push(columnEdge);
+        }
+        connectionPoint = tryConnectEdges(lastEdge, graph.at(graph.length - 1));
+    }
+    return buildGraph(connectionPoint, graph);
+}
 
-    return true;
+/**
+ * Each edge is composed by two points, if both edges share one point they are connected, 
+ * in case of sharing both they'll result in the same conceptual edge (they're the same edge),
+ * otherwise they're unconnected.
+ * @param {Edge} edge1 
+ * @param {Edge} edge2 
+ * @returns Point which connects both edges, in case of no connection null is returned instead.
+ */
+function tryConnectEdges(edge1, edge2) {
+    if (edge1.value[0].equals(edge2.value[0]) &&
+        edge1.value[1].equals(edge2.value[1]))
+        throw new Error('Both edges have the same points');
+    else if (edge1.value[0].equals(edge2.value[0]))
+        return edge1.value[0];
+    else if (edge1.value[0].equals(edge2.value[1]))
+        return edge1.value[0];
+    else if (edge1.value[1].equals(edge2.value[0]))
+        return edge1.value[1];
+    else if (edge1.value[1].equals(edge2.value[1]))
+        return edge1.value[1];
+    else
+        return null;
 }
 
 function answer_part1() {
     const input = readInput();
-    const biggestArea = findBiggestRectangle(input);
+    const biggestArea = findBiggestRectangle_v1(input);
     console.log(`Biggest possible rectangle has an area of ${JSON.stringify(biggestArea)}`);
 }
 
 answer_part1();
 
-//answer_part1();
-// const input = readInput();
-// const example = readExample();
-// console.log(JSON.stringify(example));
-// c(example);
+const example = readExample();
+findBiggestRectangle_v2(example);
